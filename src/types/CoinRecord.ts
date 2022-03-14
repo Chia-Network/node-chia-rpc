a7d6360b47a6c619b11b2146ff87e38fc429322a
Rigidity <me@rigidnetwork.com>
2022-03-14 16:39:46 -0400

    Revert coin record spent type removal.

--- src/types/CoinRecord.ts
+++ src/types/CoinRecord.ts
@@ -4,6 +4,7 @@ export interface CoinRecord {
     coin: Coin;
     confirmed_block_index: number;
     spent_block_index: number;
+    spent: boolean;
     coinbase: boolean;
     timestamp: number;
 }
