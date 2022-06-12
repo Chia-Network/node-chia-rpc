d212cc24907ac19d15037576a92c7ff2c02fa197
Rigidity <me@rigidnetwork.com>
2022-06-12 17:23:57 -0400

    Add spend bundle aggregation

---
+++ src/utils/spend.ts
@@ -0,0 +1,17 @@
+import { AugSchemeMPL, JacobianPoint } from '@rigidity/bls-signatures';
+import { SpendBundle } from '../types/SpendBundle';
+
+export function aggregateSpendBundles(
+    spendBundles: SpendBundle[]
+): SpendBundle {
+    return {
+        coin_spends: spendBundles
+            .map((spendBundle) => spendBundle.coin_spends)
+            .flat(),
+        aggregated_signature: AugSchemeMPL.aggregate(
+            spendBundles.map((spendBundle) =>
+                JacobianPoint.fromHexG2(spendBundle.aggregated_signature)
+            )
+        ).toHex(),
+    };
+}
